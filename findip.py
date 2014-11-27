e6e63543f95644ad0d554c9c397601d5f1d879a2
usrtmp <monistrator@gmail.com>
2014-11-27 13:33:49 +0800

    add python3 support

--- findip.py
+++ findip.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
-import os,time,httplib,sys,multiprocessing
+import os,time,sys,multiprocessing
+
+# python3 support, httplib renamed to http.client in python3
+if sys.version_info >= (3, 0):
+    import http.client
+else:
+    import httplib
 
 # good google's ip [elapse time, ip]
 ip_queue=multiprocessing.Queue()
@@ -14,15 +20,21 @@ thread_id=[]
 def iplookup(ip,q):
     """ Check google's ip is suitable for goagent. """
     try:
-        c=httplib.HTTPSConnection(ip,timeout=3)
-        c.request("GET", "/")
+        # python3 will use http.client.HTTPSConnection
+        if sys.version_info >= (3, 0):
+            conn=http.client.HTTPSConnection(ip,timeout=3)
+        else:
+            conn=httplib.HTTPSConnection(ip,timeout=3)
+
+        conn.request("GET", "/")
         st=time.time()
-        response=c.getresponse()
+        response=conn.getresponse()
         en=time.time()
         result=str(response.status)+' '+response.reason
         if '200 OK' in result:
             q.put([en-st, ip])
-            print ip, 'is Good'
+            print(ip+' is Good')
+            conn.close()
     except:
         pass
 
@@ -46,18 +58,18 @@ def deal_ip():
 
     # print arranged good ip, fast ahead
     if format_ip:
-        print "\r\nOutput:\r\n"
-        print format_ip[:-1]
-        print "\r\nPlease wait about 3 minutes to use goagent!!!\r\n"+\
+        print("\r\nOutput:\r\n")
+        print(format_ip[:-1])
+        print("\r\nPlease wait about 3 minutes to use goagent!!!\r\n"+\
             "I don't know why, but the scanning does affect it.\r\n"+\
             "You may have a cup of coffee, ^_^\r\n"+\
-            "Enjoy!"
-        file=open('out.txt', 'w')
-        file.write(format_ip[:-1]+'\r\n')
-        file.close()
+            "Enjoy!")
+        write_file=open('out.txt', 'w')
+        write_file.write(format_ip[:-1]+'\r\n')
+        write_file.close()
     else:
-        print "\r\nI am sorry, nothing found out"
-        print "You may need to rerun it, and wait for it more time."
+        print("\r\nI am sorry, nothing found out")
+        print("You may need to rerun it, and wait for it more time.")
 
 if __name__ == '__main__':
 
@@ -65,18 +77,21 @@ if __name__ == '__main__':
     ippool_files=["./ippool/"+s for s in os.listdir("./ippool")]
 
     try:
-        for file in ippool_files:
-            with open(file, 'r') as infile:
+        for ip_file in ippool_files:
+            with open(ip_file, 'r') as infile:
                 lines=infile.readlines()
+                # close opened file
+                infile.close()
+                # split text to list with '\n' end
                 lines=[x.rstrip('\n') for x in lines]
-                # split ip to 2d array
-                ip_all=[lines[i:i+thread_number] for i in xrange(0,len(lines),
+                # split ip to 2d array every read_number
+                ip_all=[lines[i:i+thread_number] for i in range(0,len(lines),
                             thread_number)]
                 
                 for ips in ip_all:
                     format_str="scan from "
                     format_str+="{:>15}    to    {:>15} ++please be patient"
-                    print format_str.format(ips[0],ips[-1])
+                    print(format_str.format(ips[0],ips[-1]))
                     for ip in ips:
                         p = multiprocessing.Process(target=iplookup,
                                                     args=(ip,ip_queue,))
@@ -94,17 +109,16 @@ if __name__ == '__main__':
         
                     # clean thread_id
                     thread_id=[]
+                
             
         # deal with ip
         deal_ip()
-        infile.close()
-
+        # quit
         sys.exit(0)
 
     except KeyboardInterrupt:
 
         # deal with ip
-        deal_ip()   
-        infile.close()
-
+        deal_ip()
+        # quit   
         sys.exit(0)
